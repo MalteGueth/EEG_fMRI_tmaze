c4a57da0c8bd213bb6f66db8364654b90b6c4409
Malte Rudo Güth <maltegueth@gmail.com>
2019-01-07 16:07:52 -0500

    fixed formating/indexing issues

--- adaptive_gradient_correction/marker_detection.m
+++ adaptive_gradient_correction/marker_detection.m
@@ -1,19 +1,20 @@
 %{
 function marker_detection
+
 This function detects a specified marker for the TR of an MR sequence 
 in an event channel.
 
 REQUIRED INPUTS
-events - A cell array with event labels in the first and respective 
-sample points in the seconds column
+events - A cell array with samples in the first and respective 
+event labels in the second column
 TR_trigger - The name of the TR marker as a string.
 
 OUTPUT
-gradient_artifacts - The onsets in sample points for all gradient 
+artifactOnsets - The onsets in sample points for all gradient 
 artifacts indicated by the TR markers.
 %}
 
-function [gradient_artifacts] = marker_detection(events, TR_trigger)
+function [artifactOnsets] = marker_detection(events, TR_marker)
 
-artifact_onsets = strcmp(events(1,:), TR_trigger);
-gradient_artifacts = events(2,artifact_onsets);
+artifacts = strcmp([events{:,2}]', TR_marker);
+artifactOnsets = [events{artifacts,1}]';
