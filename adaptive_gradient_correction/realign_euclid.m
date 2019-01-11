fad799ad120d7e820483b716d7a80858f5b05684
Malte Rudo Güth <maltegueth@gmail.com>
2019-01-11 15:11:27 -0500

    Update realign_euclid.m

--- adaptive_gradient_correction/realign_euclid.m
+++ adaptive_gradient_correction/realign_euclid.m
@@ -1,7 +1,7 @@
 function accel=realign_euclid(realignment_param)
 
 % Create an array of zeros the same size as the relignment parameters
-accel=zeros(1,realignment_param);
+accel=zeros(1,size(realignment_param,1));
 for vector=1:size(realignment_param,1) % Loop over every vector in the parameter array,
 									   % so every set of x,y,z coordinates
     accel(vector)=norm(realignment_param(vector,:)); % Use euclidian norm to convert the vectors
